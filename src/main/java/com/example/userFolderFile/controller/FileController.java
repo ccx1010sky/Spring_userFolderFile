e24ba39dea44d532e6061d27f3400cb99b8c9842
ccx1010sky <ccx1010@gmail.com>
2022-02-22 22:08:56 +0000

    setup User.java

--- src/main/java/com/example/userFolderFile/controller/FileController.java
+++ src/main/java/com/example/userFolderFile/controller/FileController.java
@@ -1,4 +1,13 @@
 package com.example.userFolderFile.controller;
 
+import com.example.userFolderFile.repository.FileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FileController {
+    @Autowired
+    FileRepository fileRepository;
+
+
 }
--- src/main/java/com/example/userFolderFile/models/User.java
+++ src/main/java/com/example/userFolderFile/models/User.java
@@ -1,4 +1,58 @@
 package com.example.userFolderFile.models;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @OneToMany(mappedBy = "user",fetch = FetchType.LAZY)
+    @JsonIgnoreProperties
+    private List<Folder> folders;
+
+
+//setup constructor;
+    public User(String name) {
+        this.name = name;
+        //setup empty ArrayList folders
+        this.folders = new ArrayList<>();
+    }
+//setup none constructor
+    public User() {
+    }
+//setup Id getter and setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+//setup property getter and setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Folder> getFolders() {
+        return folders;
+    }
+
+    public void setFolders(List<Folder> folders) {
+        this.folders = folders;
+    }
 }
